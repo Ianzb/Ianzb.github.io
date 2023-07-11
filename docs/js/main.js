695da44f9a12b8b101697133359fcbaa8d8cae5c
Ianzb <93322252@qq.com>
2023-07-11 10:53:53 +0800

    4.0.0beta

---
+++ docs/js/main.js
@@ -0,0 +1,6 @@
+
+function adjustIframe() {
+    var ifm = document.getElementById("bi_iframe");
+    ifm.height = document.documentElement.clientHeight;
+    ifm.width = document.documentElement.clientWidth;
+}

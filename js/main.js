3bd6fb6ede7297948c42ed660090a74183a7bf1d
Ianzb <93322252@qq.com>
2023-10-28 08:34:04 +0800

    4.0.5

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-
-function adjustIframe() {
-    var ifm = document.getElementById("bi_iframe");
+function iframeFullScreen() {
+    var ifm = document.getElementByClass("iframe-full-screen");
     ifm.height = document.documentElement.clientHeight;
     ifm.width = document.documentElement.clientWidth;
 }
+
+function copyString(string) {
+    var input = document.createElement("input");
+    input.value = string;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand("copy");
+    document.body.removeChild(input);
+=}

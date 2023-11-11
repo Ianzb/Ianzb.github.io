ea8309b2373d695f73b99af6260fd2900bc3ced1
Ianzb <93322252@qq.com>
2023-11-11 14:02:55 +0800

    test

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-function iframeFullScreen() {
-    var ifm = document.getElementById("iframe-full-screen");
-    ifm.height = document.documentElement.clientHeight;
-    ifm.width = document.documentElement.clientWidth;
-}
-
 function copyString(string) {
     var input = document.createElement("input");
     input.value = string;

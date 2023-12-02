a1a07069fa137091fc08f64bf71c823761987ffe
Ianzb <93322252@qq.com>
2023-12-02 13:55:13 +0800

    4.2.0

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ function copyString(string) {
     document.body.removeChild(input);
 }
 function load() {
-    $('#header').load('/template/header.html')
-    $('#footer').load('/template/footer.html')
+    $("#header").load("/template/header.html")
+    $("#footer").load("/template/footer.html")
+}
+function loadPage(name, page) {
+    $(name).load(page)
 }
